fb285eba2736985706561e7ac89f0670bc196e0e
christophersesugh <christohybrid185@gmail.com>
2023-05-27 15:31:10 +0000

    feat: add timer and improve algorithm efficiency

--- factorize.py
+++ factorize.py
@@ -1,5 +1,5 @@
 def factorize(number):
-    for i in range(2, number):
+    for i in range(2, int(number ** 0.5) + 1):
         if number % i == 0:
             print("{:d}={:d}*{:d}".format(number, number//i, i))
             break
--- factors.py
+++ factors.py
@@ -1,14 +1,21 @@
 import sys
-
+import signal
 from factorize import factorize
 
 if len(sys.argv) < 2:
     print("USAGE: factors <file>")
     sys.exit(1)
 
+def timeout_handler(signum, frame):
+    print("Timeout: Program execution exceeded the time limit")
+    sys.exit(1)
+
 file_name = sys.argv[1]
 
 try:
+    signal.signal(signal.SIGALRM, timeout_handler)
+    signal.alarm(5)
+
     with open(file_name, 'r') as file:
         lines = file.readlines()
 
@@ -17,6 +24,7 @@ try:
         try:
             number = int(line)
             factorize(number)
+            signal.alarm(0)
         except ValueError:
             print("Invalid number: ", line)
 
